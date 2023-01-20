85bdad051e77af387fb81db1ec3202a91abca59f
goldrush13 <113155743+goldrush13@users.noreply.github.com>
2023-01-19 23:21:15 -0600

    configuración del pwm y cuentas
    Me marca error en el ENABLE (linea 53, pwm.c) "expected declaration or statement at end of input"

--- PWM.c
+++ PWM.c
@@ -1,27 +1,53 @@
-
+// USANDO:  M0PWM4 - 49 - PG0(6) - GENERADOR 2
 #include "lib/include.h"
 
-extern void Configura_Reg_PWM1(uint16_t freq)
+extern void Configura_Reg_PWM0(uint16_t freq)
 {
-    SYSCTL->RCGCPWM |= (1<<0); /*Enable reloj de modulo PWM1 pag 354*/
-    SYSCTL->RCGCGPIO |= (1<<5); /*Enable reloj de GPIO Puerto F pag 340 pin 5*/
-   // GPIOF->AFSEL |= (1<<3)|(1<<2)|(1<<1); /*Control de registros ya sea por GPIO o Otros Pag 672*/
+    // Habilito PWM0 pg 398 (la tiva grande solo tiene 1)
+    SYSCTL->RCGCPWM |= (1<<0); 
     
-    GPIOF_AHB->AFSEL |= 0x0E;
-    GPIOF_AHB->PCTL |= (GPIOF_AHB->PCTL&0xFFFF000F) | 0x00006660; /*Combinado con la tabla Pag 1351 y el registro PCTL le digo que es pwm Pag 689*/
-    GPIOF_AHB->DEN |= (1<<3)|(1<<2)|(1<<1); /* para decirle si es digital o no Pag 682*/
-    PWM0->CC &= ~(1<<8);  /*Enable o Disable Divisor  Pag 1747*/
-    PWM0->_0_CTL = (0<<0);
-    PWM0->_1_CTL = (0<<0); /*Bloqueo y desbloqueo*/
-    PWM0->_1_GENB = 0x0000080C; /*Registro de las acciones del pwm Pag 1285*/
-    PWM0->_1_GENA = 0x0000008C; /*Registro de las acciones del pwm Pag 1282*/
-    PWM0->_0_GENB = 0x0000008C;//PWM5
-    PWM0->_1_LOAD = (int)(20000000/freq); /*cuentas=fclk/fpwm  para 1khz cuentas = (16,000,000/1000)*/
-    PWM0->_0_LOAD = (int)(20000000/freq);
-    PWM0->_1_CMPB = 15000;
-    PWM0->_1_CMPA = 15000;
-    PWM0->_0_CMPA = 15000;
-    PWM0->_1_CTL = (1<<0);// Se activa el generador 3
-    PWM0->_0_CTL = (1<<0);// Se activa el generador 2
-    PWM0->ENABLE = (1<<3) | (1<<2) | (1<<1); /*habilitar el bloque pa que pase Pag 1247*/
-}
+    // Habilito el puerto G (porque usare el pin PG0) pg 383
+    SYSCTL->RCGCGPIO |= (1<<6);
+    
+    // Habilito función alternativa para PG0 PG 770
+    GPIOG_AHB->AFSEL |= (1<<0); // 0 por PG0 jeje
+
+    // Habilito la salida de PG0
+    GPIOG_AHB->DIR = (1<<0);
+
+    // Combinación de pg 788 para ver que numero estan en el campo de bits (para PG0 es del 0 al 3)
+    //                pg 1808 le tengo que poner un 6 porque ahí esta "M0PWM4"
+    // Entonces pongo en el campo de bits 0, un 6
+    GPIOG_AHB->PCTL |= (GPIOG_AHB->PCTL&0xFFFFFFF0) | 0x00000006;
+    
+    // El pin es digital:
+    GPIOG_AHB->DEN |= (1<<0); 
+    
+    // PASO 5 Activo divisor 
+    PWM0->CC &= ~(1<<20);  /*Enable o Disable Divisor  Pag 1747*/
+    
+    
+    //////////////////// CONFIGURACION PARAMETROS PWM /////////////////
+    
+    // BLOQUEO (estoy usando generador 2)
+    PWM0->_2_CTL = (0<<0);
+
+    // Voy usar el comparador A
+    /* Action for counter = 0 --> do nothing
+       Action for counter = LOAD --> drive pwmA HIGH    */
+    PWM0->_2_GENA = 0x0000008C;         //Registro de las acciones del pwm Pag 1722 (Pte 2, min 30)
+
+    // Cargo el valor
+    /*          cuentas = fclk/fpwm  
+    para 1kHz:  cuentas = (16,000,000/1000)
+    para 50kHz: cuentas = (50 000 000/10 000) = 5 000 cuentas */
+    PWM0->_2_LOAD = 5000;
+
+    // Establecer el valor del ciclo de trabajo
+    PWM0->_2_CMPA = 3500;           // ciclo de trabajo 70%, los primeras 3500 cuentas estaran en alto, y despues va a bajar
+
+    // DESBLOQUEO
+    PWM0->_2_CTL = (1<<0);
+
+    // Habilito el modulo para que me deje pasar la señal :D
+    PWM0->ENABLE = (1<<4);          // pp 1687
